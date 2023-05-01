d3130bb7ec70c106a1006c7b0f3b2a9f08980954
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-01 23:55:24 +0300

    add social links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/Home";
+import Sociallinks from "./components/social_links/sociallinks";
 
 function App() {
   return (
     <div>
+
      <Navbar />
      <Home />
-
+     <Sociallinks />
+     
     </div>
   );
 }
