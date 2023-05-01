e2eeaead083cc62d6a39d08dc297f5c7ff1a99f8
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-01 23:54:37 +0300

    create social links and style it

---
+++ src/components/social_links/sociallinks.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sociallinksdata,{links} from './sociallinksdata';
+
+
+const Sociallinks = () => {
+  return (
+    <div className='hidden lg:flex flex-col right-0 top-[35%] fixed'>
+        <ul>
+           {links.map((data)=>(
+             <Sociallinksdata data={data} key={data.id} />
+           ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Sociallinks;
