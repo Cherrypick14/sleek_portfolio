479dc7ab0ceac0f046b1e870dfa99e32ecd42f34
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-01 11:13:50 +0300

    complete navbar responsive

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,40 @@
+import React,{useState} from 'react'
+import{FaBars, FaTimesCircle} from 'react-icons/fa'
+
+import Navlinks, {links} from '../navbar/navlinks'
+
+const Navbar = () => {
+    const [nav, setNav]= useState(false);
+
+  return (
+    <div className='fixed h-20 w-full flex justify-between items-center text-[#85D8CE] bg-[#16222A] px-4'>
+      <div>
+        <h1 className='text-5xl font-signature ml-2'>Cheryl_dev</h1>
+      </div>
+      
+      <ul className='hidden md:flex'>
+      {links.map((data)=>(
+         <Navlinks data={data} key={data.id}/>
+       ))}
+      </ul>  
+
+      <div onClick={()=> setNav(!nav)} className='cursor-pointer z-20 pr-4 text-[#3A6073] md:hidden'>
+      
+        {nav ? <FaTimesCircle size={20}/> : <FaBars size={20}/>}
+      </div>
+      
+      {nav &&(
+        <div className='absolute md:hidden h-screen top-0 left-0 w-full bg-[#16222A]'>
+            ( <ul className='flex flex-col space-y-6 items-center justify-center mt-12 sm:text-2xl text-[#85D8CE]'>
+        {links.map((data)=>(
+         <Navlinks data={data} key={data.id}/>
+       ))}
+        </ul>)
+        </div>
+
+      ) }  
+    </div>
+  )
+}
+
+export default Navbar;
