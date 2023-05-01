4f5c5f23a533541c4b6f86be42176ece99b09bd1
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-01 05:33:05 +0300

    add new project files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="font-bold text-orange-500 underline items-center justify-center flex pt-5">We are doing things different this time</h1>
+
+    </div>
+  );
+}
+
+export default App;
