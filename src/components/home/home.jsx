e6accf5acf1c6bafbdb8b9df03ae8aebe60f7ee2
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-02 16:24:09 +0300

    change naming convention

---
+++ src/components/home/home.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import gprofile from '../../assets/gProfile.jpg'
+import{MdKeyboardArrowRight} from 'react-icons/md'
+
+const Home = () => {
+    
+  return (
+    <div name="home" className='h-screen w-full bg-gradient-to-tl from-[#16222A] via-[#3A6073] to bg-[#85D8CE] '>
+       <div className='max-w-screen-lg mx-auto items-center justify-center flex flex-col h-full md:flex-row'>
+        <div className='flex flex-col justify-center h-full ml-5'>
+            <h2 className='text-3xl sm:text-5xl font-bold text-[#16222A]'>I'm a Front-end Developer</h2>
+             <p className='py-4 max-w-md text-white'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione esse molestias doloribus excepturi hic provident ipsum, sapiente quia optio eveniet tempora tenetur laborum officia inventore incidunt similique ipsam aliquam suscipit?
+             </p>
+
+             <div>
+                <button className='group text-white w-fit px-4 py-4 flex items-center rounded-md bg-gradient-to-bl from-[#3A6073] to bg-[#16222A] '>
+                    Portfolio<span className='group-hover:rotate-90'> <MdKeyboardArrowRight size={25} className='ml-1
+                    '/></span>
+                </button>
+             </div>
+
+        </div>
+
+        <div>
+            <img src={gprofile} alt="my profile" className='rounded-e-2xl w-2/3 mx-auto ' />
+        </div>
+
+       </div>
+
+    </div>
+  )
+}
+
+export default Home;
