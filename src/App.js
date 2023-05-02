45491546f631e7aa88f2b1de03703734b78f3654
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-02 18:10:39 +0300

    add portfolio component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
-import Home from "./components/home/Home";
+import Home from "./components/home/home";
 import Sociallinks from "./components/social_links/sociallinks";
 import About from "./components/about/about";
+import Portfolio from "./components/portfolio/portfolio";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
      <Navbar />
      <Home />
      <About />
+     <Portfolio />
      
      <Sociallinks />
      
