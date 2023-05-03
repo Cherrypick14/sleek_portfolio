a7948cf634cb249823e4e6a6091e922f65d2de02
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-03 07:17:51 +0300

    modify the outer div style

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -4,7 +4,7 @@ import Portfolioarray,{portfolios} from './portfolioarray';
 
 const Portfolio = () => {
   return (
-    <div name='portfolio'className='md:h-screen w-full bg-gradient-to-tl from-[#16222A] via-[#3A6073] to bg-[#85D8CE] text-white'>
+    <div name='portfolio'className='h-screen w-full bg-gradient-to-tl from-[#16222A] via-[#3A6073] to bg-[#85D8CE] text-white'>
        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-screen'>
           <div className='pb-8'>
             <p className='font-bold text-4xl border-b-4 inline text-[#16222A]'>Portfolio</p>
