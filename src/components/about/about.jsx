820c1e50b6282ff13b0495a6441dacefb9b90a90
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-04 14:22:30 +0300

    add content to about section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,11 +8,11 @@ const About = () => {
                 <h2 className='font-bold text-4xl border-b-4 inline text-[#16222A] '>About</h2>
             </div>
             <p className=' md:text-xl  md:mt-20 text-white'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione facilis magni harum ex expedita natus, obcaecati maiores numquam eius est rem iusto debitis. Fugit, quia! Voluptatibus, doloribus. In, autem cupiditate!
+            Well,as an avid hiker,I have developed skills in planning, preparation and navigation. This activity has taught me how to build resilience,perseverance and problem-solving, which can be applicable in a professional setting.
             </p>
             <br/>
             <p className='md:text-xl text-white '>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quis cupiditate iusto unde itaque debitis culpa quod quos ratione animi beatae aliquid eius fugit obcaecati, sunt, aspernatur voluptatibus quisquam nobis?
+            Participating in hackathons can be a great way to gain experience working collaboratively on real-world projects. In the Value Chain Factory Hackathon, I had the opportunity to contribute as a front-end engineer to a team developing solutions for various sectors. Specifically, I worked on a project aimed at addressing emergency needs in the health care sector. Through the use of the React-Django-Postgres tech stack, I developed a website that helped improve emergency response in the health care sector. This experience allowed me to further develop my front-end engineering skills while also making a tangible impact in the community.
             </p>
         </div>
     </div>
