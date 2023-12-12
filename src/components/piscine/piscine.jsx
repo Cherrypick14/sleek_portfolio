a3cfcfbeee14e3aecbc464e49e9d5532b47956cd
Cherrypick14 <cherrypicklet@gmail.com>
2023-12-12 22:04:51 +0300

    add piscine xp

---
+++ src/components/piscine/piscine.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Piscine = () => {
+  return (
+    <div name="about" className='h-screen w-full bg-gradient-to-tl from-[#16222A] via-[#3A6073] to bg-[#85D8CE]'>
+        <div className='max-w-screen-lg h-screen w-full justify-center flex flex-col p-4 mx-auto'>
+            <div className='pb-4'>
+                <h2 className='font-bold text-4xl border-b-4 inline text-[#16222A] '>Piscine - Nurturing Growth in a Coding Odyssey</h2>
+            </div>
+            <p className=' md:text-xl  md:mt-20 text-white'>
+            Embarking on the Piscine, a rigorous four-week in-person coding interview, proved to be a transformative experience that not only honed my technical skills but also fostered adaptability and resilience. Navigating an array of coding challenges, I swiftly adapted to the demanding pace, showcasing not only problem-solving prowess but also creativity and ingenuity.
+            </p>
+            <br/>
+            <p className='md:text-xl text-white '>
+            The Piscine went beyond assessing coding abilities; it delved into thought processes, encouraging a growth mindset and collaboration with peers. This coding crucible not only prepared me for the dynamic challenges of software development but instilled a profound appreciation for iterative problem-solving, shaping my approach to coding with confidence and vigor.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Piscine;
