6891f5e82b327e68d6cbc29ff23e9145d564f091
Cherrypick14 <cherrypicklet@gmail.com>
2023-12-12 22:21:36 +0300

    switch name from portfolio to projects

--- src/components/portfolio/projects.jsx
+++ src/components/portfolio/projects.jsx
@@ -7,7 +7,7 @@ const Portfolio = () => {
     <div name='portfolio'className='h-screen w-full bg-gradient-to-tl from-[#16222A] via-[#3A6073] to bg-[#85D8CE] text-white'>
        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-screen'>
           <div className='pb-8'>
-            <p className='font-bold text-4xl border-b-4 inline text-[#16222A]'>Portfolio</p>
+            <p className='font-bold text-4xl border-b-4 inline text-[#16222A]'>Projects</p>
             <p className='py-6'>Check some of my passionate projects!!!...</p>
           </div>
           <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
