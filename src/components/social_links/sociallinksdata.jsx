5f04d4be248c7a94224a52dacea540e476995afb
Cherrypick14 <cherrypicklet@gmail.com>
2023-12-12 20:04:36 +0300

    redo twitter icon

--- src/components/social_links/sociallinksdata.jsx
+++ src/components/social_links/sociallinksdata.jsx
@@ -3,6 +3,7 @@ import {FaGithub,FaLinkedin} from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Twitter from '../logoss/twitter'
 
 export const links = [
     {
@@ -19,8 +20,8 @@ export const links = [
         id:2,
         child:(
             <>
-            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
-           X <FontAwesomeIcon icon="fa-brands fa-x-twitter" size={30} />
+         
+           X <FontAwesomeIcon icon={Twitter} size={30} />
           {/* Twitter  <FaTwitter size={30} />    */}
             </>
         ),
