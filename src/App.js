5621cbb93aa555c2fbf74bfc8fd1a7f0ed058a43
Cherrypick14 <cherrypicklet@gmail.com>
2023-12-13 12:20:21 +0300

    simple component fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
 import Sociallinks from "./components/social_links/sociallinks";
-import Piscine from "./components/piscine/piscine";
 import Portfolio from "./components/portfolio/portfolio";
 import Technologies from "./components/technologies/technologies";
 import Contacts from "./components/contacts/contacts";
+import Piscine from "./components/piscine/piscine";
 
 function App() {
   return (
