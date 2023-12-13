203224a0e1896641f8fbcb0a54d89d6fc0409d24
Cherrypick14 <cherrypicklet@gmail.com>
2023-12-13 12:17:43 +0300

    add new desc

--- src/components/piscine/piscine.jsx
+++ src/components/piscine/piscine.jsx
@@ -5,7 +5,7 @@ const Piscine = () => {
     <div name="about" className='h-screen w-full bg-gradient-to-tl from-[#16222A] via-[#3A6073] to bg-[#85D8CE]'>
         <div className='max-w-screen-lg h-screen w-full justify-center flex flex-col p-4 mx-auto'>
             <div className='pb-4'>
-                <h2 className='font-bold text-4xl border-b-4 inline text-[#16222A] '>Piscine - Nurturing Growt in a Coding Odyssey</h2>
+                <h2 className='font-bold text-4xl border-b-4 inline text-[#16222A] '>Piscine - Nurturing Growth in a Coding Odyssey</h2>
             </div>
             <p className=' md:text-xl  md:mt-20 text-white'>
             Embarking on the Piscine, a rigorous four-week in-person coding interview, proved to be a transformative experience that not only honed my technical skills but also fostered adaptability and resilience. Navigating an array of coding challenges, I swiftly adapted to the demanding pace, showcasing not only problem-solving prowess but also creativity and ingenuity.
