ac9c7b9d53fdf4a2221a6a474499242026d47361
Cherrypick14 <cherrypicklet@gmail.com>
2023-12-14 17:35:53 +0300

    fixing new typos

--- src/components/portfolio/projects.jsx
+++ src/components/portfolio/projects.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Projectsarray,{projects} from './projectsarray';
 
 
-const Portfolio = () => {
+const Project = () => {
   return (
-    <div name='portfolio'className='h-screen w-full bg-gradient-to-tl from-[#16222A] via-[#3A6073] to bg-[#85D8CE] text-white'>
+    <div name='project'className='h-screen w-full bg-gradient-to-tl from-[#16222A] via-[#3A6073] to bg-[#85D8CE] text-white'>
        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-screen'>
           <div className='pb-8'>
             <p className='font-bold text-4xl border-b-4 inline text-[#16222A]'>Projects</p>
@@ -21,4 +21,4 @@ const Portfolio = () => {
   )
 }
 
-export default Portfolio;
+export default Project;
