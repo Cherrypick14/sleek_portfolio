223eda832a2dac8ece5932e87fa9e107b33ca413
Cherrypick14 <cherrypicklet@gmail.com>
2023-12-14 17:52:00 +0300

    fixing issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
 import Sociallinks from "./components/social_links/sociallinks";
-import Portfolio from "./components/portfolio/portfolio";
+import Project from "./components/portfolio/projects";
 import Technologies from "./components/technologies/technologies";
 import Contacts from "./components/contacts/contacts";
 import Piscine from "./components/piscine/piscine";
@@ -14,7 +14,7 @@ function App() {
      <Navbar />
      <Home />
      <Piscine />
-     <Portfolio />
+     <Project />
      <Technologies />
      <Contacts />
      
