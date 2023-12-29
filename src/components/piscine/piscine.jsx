2247013b1772fe1b2c860b995062392ee507e989
Cherrypick14 <cherrypicklet@gmail.com>
2023-12-29 22:44:44 +0300

    change a few context of the piscine

--- src/components/piscine/piscine.jsx
+++ src/components/piscine/piscine.jsx
@@ -8,11 +8,11 @@ const Piscine = () => {
                 <h2 className='font-bold text-4xl border-b-4 inline text-[#16222A] '>Piscine - Nurturing Growth in a Coding Odyssey</h2>
             </div>
             <p className=' md:text-xl  md:mt-20 text-white'>
-            Embarking on the Piscine, a rigorous four-week in-person coding interview @zone01_Kisumu, proved to be a transformative experience that not only honed my technical skills but also fostered adaptability and resilience. Navigating an array of coding challenges, I swiftly adapted to the demanding pace, showcasing not only problem-solving prowess but also creativity and ingenuity.
+            Embarking on the Piscine, an intensive four-week hands-on experience at @zone01_Kisumu, turned out to be a game-changer. It was more than just a coding immersion; it transformed me by refining technical skills and nurturing adaptability and resilience. Facing a variety of challenges, I quickly adjusted to the fast-paced environment, demonstrating not only problem-solving abilities but also unleashing creativity and ingenuity.
             </p>
             <br/>
             <p className='md:text-xl text-white '>
-            The Piscine went beyond assessing coding abilities; it delved into thought processes, encouraging a growth mindset and collaboration with peers. This coding crucible not only prepared me for the dynamic challenges of software development but instilled a profound appreciation for iterative problem-solving, shaping my approach to coding with confidence and vigor.
+            The Piscine wasn't just about evaluating coding skills; it dove into thought processes, fostering a growth mindset and collaboration among peers. This coding experience not only readied me for the ever-changing landscape of software development but also instilled a deep appreciation for iterative problem-solving. It significantly influenced my coding approach, imbuing it with confidence and vigor.
             </p>
         </div>
     </div>
